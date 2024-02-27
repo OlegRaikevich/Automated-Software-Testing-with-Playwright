f863a4e4cfc4605c6eb1335143492331f4697c29
olegraikevich <oraikevich@sardinasystems.com>
2024-02-27 13:14:58 +0400

    add Currency Exchange test suite

---
+++ tests/e2e/e2e-currency-exchange.spec.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "@playwright/test"
+import { login } from "../../helpers"
+
+test.describe.only("Currency Exchange", () => {
+    test.beforeEach(async ({ page }) => {
+        await login(page)
+        await page.goto('http://zero.webappsecurity.com/bank/pay-bills.html')
+    })
+
+    test("Purchase Foreign Currency", async ({ page }) => {
+        await page.click('a[href="#ui-tabs-3"]')
+        await page.pause()
+        await page.selectOption("#pc_currency", "EUR")
+        await page.pause()
+        await page.type("#pc_amount", '500')
+        await page.click("#pc_inDollars_true")
+        await page.click('#purchase_cash')
+
+        const succsesfullPurchase = page.locator('#alert_content')
+        await expect(succsesfullPurchase).toBeVisible()
+        await expect(succsesfullPurchase).toContainText('Foreign currency cash was successfully purchased.')
+    })
+})
