82f831e1f90b7fbc67932f219ae7a0236a1f4a44
olegraikevich <oraikevich@sardinasystems.com>
2024-03-01 07:55:14 +0400

    cleanup project code

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,23 +1,26 @@
 import { test, expect } from '@playwright/test'
 import { LoginPage } from '../../page-objects/LoginPage'
+import { HomePage } from '../../page-objects/HomePage'
 
 test.describe.parallel("Login / Logout Flow", () => {
     let loginPage: LoginPage
+    let homePage: HomePage
 
     test.beforeEach(async ({ page }) => {
         loginPage = new LoginPage(page)
-        await loginPage.visit()
+        homePage = new HomePage(page)
+
+        await homePage.visit()
     })
-    // Negatie scenario
 
     test('Negative Scenario for Login', async ({ page }) => {
-        await page.click('#signin_button')
+        await homePage.clickOnSignIn()
         await loginPage.login('invalid username', 'invalid password')
         await loginPage.assertErrorMessage()
     })
-    // Positive Scenario + Logout
+    
     test('Positive Scenario for Login + Logout', async ({ page }) => {
-        await page.click('#signin_button')
+        await homePage.clickOnSignIn()
         await loginPage.login('username', 'password')
 
         await page.goBack()
