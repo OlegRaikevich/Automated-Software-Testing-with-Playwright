488e2c4f711ab00ddac8521a27169d1953e97d27
Oleg Raikevich <olegraykevich@github.com>
2024-03-14 23:39:44 +0400

    refactor e2e-filter-transactions tests

--- tests/e2e/e2e-filter-transactions.spec.ts
+++ tests/e2e/e2e-filter-transactions.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from "@playwright/test"
-import { HomePage } from "../../page-objects/HomePage"
 import { LoginPage } from "../../page-objects/LoginPage"
+import { HomePage } from "../../page-objects/HomePage"
 
-test.describe.parallel.only("Filter Transactions", () => {
+test.describe.parallel("Filter Transactions", () => {
     let homePage: HomePage
     let loginPage: LoginPage
 
@@ -13,15 +13,10 @@ test.describe.parallel.only("Filter Transactions", () => {
         homePage.visit()
         homePage.clickOnSignIn()
         loginPage.login('username', 'password')
-        await page.pause()
     })
 
-    test.only("Transactions Savings account", async ({ page }) => {
-        await page.pause()
-        await page.click("#online-banking")
-        await page.pause()
+    test("Transactions Savings account", async ({ page }) => {
         await page.click("#account_activity_link")
-        await page.pause()
         await page.selectOption("#aa_accountId", "1")
 
         const tableRowDate = page.locator("#all_transactions_for_account thead th:nth-child(1)")
@@ -39,7 +34,7 @@ test.describe.parallel.only("Filter Transactions", () => {
     })
 
     test("Transactions Checking account", async ({ page }) => {
-        await page.click('#account_activity_link')
+        await page.click("#account_activity_link")
         await page.selectOption("#aa_accountId", "2")
 
         const tableRowDate = page.locator("#all_transactions_for_account thead th:nth-child(1)")
@@ -57,7 +52,7 @@ test.describe.parallel.only("Filter Transactions", () => {
     })
 
     test("Transactions Loan account", async ({ page }) => {
-        await page.click('#account_activity_link')
+        await page.click("#account_activity_link")
         await page.selectOption("#aa_accountId", "4")
 
         const tableRowDate = page.locator("#all_transactions_for_account thead th:nth-child(1)")
@@ -75,7 +70,7 @@ test.describe.parallel.only("Filter Transactions", () => {
     })
 
     test("Transactions Creadit Card account", async ({ page }) => {
-        await page.click('#account_activity_link')
+        await page.click("#account_activity_link")
         await page.selectOption("#aa_accountId", "5")
 
         const noResults = page.locator("div.well")
@@ -84,7 +79,7 @@ test.describe.parallel.only("Filter Transactions", () => {
     })
 
     test("Transactions Brokerage account", async ({ page }) => {
-        await page.click('#account_activity_link')
+        await page.click("#account_activity_link")
         await page.selectOption("#aa_accountId", "6")
 
         const noResults = page.locator("div.well")
