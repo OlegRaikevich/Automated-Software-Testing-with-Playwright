c0876dc50d44e3f821a054442c5791dec081bb22
Oleg Raikevich <olegraykevich@github.com>
2024-03-15 21:55:35 +0400

    refactor payment test with page object model

--- tests/e2e/e2e-payment.spec.ts
+++ tests/e2e/e2e-payment.spec.ts
@@ -1,14 +1,21 @@
 import { test, expect } from "@playwright/test"
 import { LoginPage } from '../../page-objects/LoginPage'
 import { HomePage } from '../../page-objects/HomePage'
+import { PaymentPage } from "../../page-objects/PaymentPage"
+import { Navbar } from "../../page-objects/components/Navbar"
 
-test.describe("Payment", () => {
+test.describe.only("Payment", () => {
     let homePage: HomePage
     let loginPage: LoginPage
+    let paymentPage: PaymentPage
+    let navbar: Navbar
 
     test.beforeEach(async ({ page }) => {
         homePage = new HomePage(page)
         loginPage = new LoginPage(page)
+        paymentPage = new PaymentPage(page)
+        navbar = new Navbar(page)
+
 
         homePage.visit()
         homePage.clickOnSignIn()
@@ -18,19 +25,9 @@ test.describe("Payment", () => {
     test("New payment", async ({ page }) => {
         await page.click('#online-banking')
         await page.click('#pay_bills_link')
-        await page.selectOption("#sp_payee", 'apple')
-        await page.click('#sp_get_payee_details')
-        await page.waitForSelector('#sp_payee_details')
 
-        await page.selectOption('#sp_account', '6')
-        await page.type('#sp_amount', '5000')
-        await page.type('#sp_date', '2024-02-08')
-        await page.type('#sp_description', 'test description')
-        await page.click('#pay_saved_payees')
-
-        const successfullMeasage = page.locator('#alert_content > span')
-        await expect(successfullMeasage).toBeVisible()
-        await expect(successfullMeasage).toContainText('The payment was successfully submitted.')
+        await paymentPage.createPyment()
+        await paymentPage.assertSuccessfullMeasage()
     })
 
 })
