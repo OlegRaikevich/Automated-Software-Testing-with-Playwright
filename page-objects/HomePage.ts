497fd16f4ce92b5527d2553e627a7e8a7ca3982b
olegraikevich <oraikevich@sardinasystems.com>
2024-03-21 03:49:57 +0400

    update HomePage class

--- page-objects/HomePage.ts
+++ page-objects/HomePage.ts
@@ -5,15 +5,25 @@ export class HomePage {
     readonly signInButton: Locator
     readonly searchBox: Locator
     readonly linkFeedbackTab: Locator
+    readonly linkOnlineBankingTab: Locator
+    readonly moreServicesButton: Locator
+    readonly linkAccountActivity: Locator
+    readonly linkTransferFunds: Locator
+    readonly linkMoneyMap: Locator
 
     constructor(page: Page) {
         this.page = page
         this.signInButton = page.locator('#signin_button')
         this.searchBox = page.locator('#searchTerm')
         this.linkFeedbackTab = page.locator('#feedback')
+        this.linkOnlineBankingTab = page.locator('#onlineBankingMenu')
+        this.moreServicesButton = page.locator('#online-banking')
+        this.linkAccountActivity = page.locator('#account_activity_link')
+        this.linkTransferFunds = page.locator('#transfer_funds_link')
+        this.linkMoneyMap = page.locator('#money_map_link')
     }
 
-    async visit() {
+    async visitHomePage() {
         await this.page.goto('http://zero.webappsecurity.com/')
     }
 
@@ -21,13 +31,33 @@ export class HomePage {
         await this.signInButton.click()
     }
 
+    async searchFor(phrase: string) {
+        await this.searchBox.type(phrase)
+        await this.page.keyboard.press('Enter')
+    }
+
     async clickOnFeedbackLink() {
         await this.linkFeedbackTab.click()
     }
 
-    async searchFor(phrase: string) {
-        await this.searchBox.type(phrase)
-        await this.page.keyboard.press('Enter')
+    async clickOnOnlineBankingLink() {
+        await this.linkOnlineBankingTab.click()
+    }
+
+    async clickOnMoreServicesButton() {
+        await this.moreServicesButton.click()
+    }
+
+    async clickOnAccountActivityLink() {
+        await this.linkAccountActivity.click()
+    }
+
+    async clickOnTransferFunds() {
+        await this.linkTransferFunds.click()
+    }
+
+    async clickOnMoneyMap() {
+        await this.linkMoneyMap.click()
     }
 }
 
