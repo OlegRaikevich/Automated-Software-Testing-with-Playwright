974ee6713e6b9a3d2684a5f085a23a655ee00f64
olegraikevich <oraikevich@sardinasystems.com>
2024-03-21 05:45:03 +0400

    update payment test

--- tests/e2e/e2e-payment.spec.ts
+++ tests/e2e/e2e-payment.spec.ts
@@ -3,18 +3,21 @@ import { LoginPage } from '../../page-objects/LoginPage'
 import { HomePage } from '../../page-objects/HomePage'
 import { PaymentPage } from "../../page-objects/PaymentPage"
 import { AccountNavigationBar } from "../../page-objects/components/AccountNavigationBar"
+import { OnlineBankingPage } from "../../page-objects/OnlineBankingPage"
 
-test.describe.only("Payment", () => {
+test.describe("Payment", () => {
     let homePage: HomePage
     let loginPage: LoginPage
     let paymentPage: PaymentPage
     let navbar: AccountNavigationBar
+    let onlineBankingPage: OnlineBankingPage
 
     test.beforeEach(async ({ page }) => {
         homePage = new HomePage(page)
         loginPage = new LoginPage(page)
         paymentPage = new PaymentPage(page)
         navbar = new AccountNavigationBar(page)
+        onlineBankingPage = new OnlineBankingPage(page)
 
 
         homePage.visitHomePage()
@@ -23,8 +26,8 @@ test.describe.only("Payment", () => {
     })
 
     test("New payment", async ({ page }) => {
-        await page.click('#online-banking')
-        await page.click('#pay_bills_link')
+        await homePage.clickOnOnlineBankingLink()
+        await onlineBankingPage.clickOnPayBillsLink()
 
         await paymentPage.createPyment()
         await paymentPage.assertSuccessfullMeasage()
