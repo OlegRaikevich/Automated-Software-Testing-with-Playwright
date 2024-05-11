8f44ced2329164d894bc53962e9e4b6c8712b7f1
olegraikevich <oraikevich@sardinasystems.com>
2024-05-11 21:37:57 +0400

    update configs

--- visual.config.ts
+++ visual.config.ts
@@ -3,7 +3,7 @@ import { PlaywrightTestConfig } from "@playwright/test"
 const config: PlaywrightTestConfig = {
     timeout: 30000,
     retries: 0,
-    testDir: "tests/visual",
+    testDir: "tests/zerobank/visual",
     use: {
         headless: true,
         viewport: { width: 1280, height: 720 },
