3c9b1d22ccfda0240cea78e624bb1ef8de48ca5d
Oleg Raikevich <olegraykevich@github.com>
2024-05-15 00:10:17 +0400

    add classes for repositories pages

---
+++ page-objects/github.com/RepositoriesPage.ts
@@ -0,0 +1,15 @@
+import {expect, Locator, Page} from "@playwright/test"
+
+export class RepositoriesPage {
+    readonly page: Page
+    readonly newRepositoryButton: Locator
+
+    constructor(page: Page) {
+        this.page = page
+        this.newRepositoryButton = page.locator('.text-center.btn.btn-primary.ml-2')
+    }
+
+    async clickOnNewRepositoryButton() {
+        await this.newRepositoryButton.click()
+    }
+}
