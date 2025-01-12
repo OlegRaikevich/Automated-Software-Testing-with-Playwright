97c8041255f66426e10f32a4389a99f59181645e
olegraikevich <olegraikevich@gmail.com>
2025-01-12 17:58:52 +0400

    add some usefull methods to page objects

--- page-objects/LoginPage.ts
+++ page-objects/LoginPage.ts
@@ -14,13 +14,4 @@ export class LoginPage extends BasePage {
         await this.fillInput(Locators.loginPage.passwordInput, password || "defaultPassword")
         await this.click(Locators.loginPage.signInButton)
     }
-
-    // async assertLoginErrorAllert() {
-    //     await expect(this.loginAllert).toBeVisible()
-    //     await expect(this.loginAllert).toContainText(" Incorrect username or password. ")
-    // }
-
-    // async assertLoginSuccess() {
-    //     await expect(this.page).toHaveURL("https://github.com/session")
-    // }
 }
--- page-objects/RepositoriesPage.ts
+++ page-objects/RepositoriesPage.ts
@@ -5,4 +5,14 @@ export class RepositoriesPage extends BasePage {
     async clickOnNewRepositoryButton() {
         await this.click(Locators.repositoriesPage.newRepositoryButton)
     }
+
+    async deleteRepository(repoName: string){
+        await this.click(Locators.repositoriesPage.settingsTab)
+
+        await this.page.locator(Locators.repositoriesPage.deleteRepositoryButton).scrollIntoViewIfNeeded()
+        await this.click(Locators.repositoriesPage.deleteRepositoryButton)
+
+        await this.fillInput(Locators.repositoriesPage.confirmDeleteInput, repoName)
+        await this.click(Locators.repositoriesPage.confirmDeleteButton)
+    }
 }
